9cfba49aae3d99cd0bba7cc871afc53b639c95a8
kgSuzuki1994 <takuti0511@gmail.com>
2019-12-29 15:17:17 +0900

    [add] 1秒ごとに数値を加算

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -7,6 +7,12 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    // 1度だけ代入するものはvalを使う
+    val handler = Handler()
+    // 繰り返し代入するためvarを使う
+    var timeValue = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +21,13 @@ class MainActivity : AppCompatActivity() {
         val startButton = findViewById(R.id.start) as Button
         val stopButton = findViewById(R.id.stop) as Button
         val resetButton = findViewById(R.id.reset) as Button
+
+        // 1秒ごとに処理を実行
+        val runnable = object : Runnable {
+            override fun run() {
+                timeValue++
+                handler.postDelayed(this, 1000)
+            }
+        }
     }
 }
