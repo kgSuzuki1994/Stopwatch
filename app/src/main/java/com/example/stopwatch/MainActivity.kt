7e14f38f5576f6fa4cdccca09d1a00391c84fc9a
kgSuzuki1994 <takuti0511@gmail.com>
2019-12-29 15:27:02 +0900

    [add] TextViewの表示を更新する

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -26,8 +26,30 @@ class MainActivity : AppCompatActivity() {
         val runnable = object : Runnable {
             override fun run() {
                 timeValue++
+                // TextViewを更新
+                // ?.letを用いて、nullでない場合のみ更新
+                timeToText(timeValue)?.let {
+                    // timeToText(timeValue)の値がlet内ではitとして使える
+                    timeText.text = it
+                }
                 handler.postDelayed(this, 1000)
             }
+        }
+    }
+
+    // 数値を00:00:00形式の文字列に変換する関数
+    // 引数timeにはデフォルト値0を設定、返却する方はnullableなString?型
+    private fun timeToText(time: Int = 0): String? {
+        // if式は値を返すため、そのままreturnできる
+        return if (time < 0) {
+            null
+        } else if (time == 0) {
+            "00:00:00"
+        } else {
+            val h = time /3600
+            val m = time % 3600 / 60
+            val s = time % 60
+            "%1$02d:%2&02d:%3$02d".format(h, m, s)
         }
     }
 }
